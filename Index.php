1841668aff4eeb64927461c45d460c89d150efcb
Bhuvnesh sharma <bhuveneshsharma27@gmail.com>
2020-12-05 21:42:56 -0800

    Initial commit

---
+++ Index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Instamojo Gateway Integration</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+	<body>
+		
+		<section class="pricing" id="pricingdiv">
+			<div class="container">
+				<h1>KIDS CLASSES</h1>
+			</div>
+			<div class="container grid-4">
+				<div class="card">
+					<div class="card-header">BASIC</div>
+					<div class="card-body">
+						<li>₹<span class="money">1200</span>/Month</li>
+						<li>Responsiive Website</li>
+						<li>Domain Name Free</li>
+						<li>Mobile Friendly</li>
+						<li>Webmail Support</li>
+						<li>Customer Support 24/7</li>
+					</div>
+					<div class="card-footer">
+						<a href="#">Purchase</a>
+					</div>
+				</div>
+				<div class="card">
+					<div class="card-header">DEFENCE</div>
+					<div class="card-body">
+						<li>₹<span class="money">3200</span>/2 Month</li>
+						<li>Responsiive Website</li>
+						<li>Domain Name Free</li>
+						<li>Mobile Friendly</li>
+						<li>Webmail Support</li>
+						<li>Customer Support 24/7</li>
+					</div>
+					<div class="card-footer">
+						<a href="#">Purchase</a>
+					</div>
+				</div>
+				<div class="card">
+					<div class="card-header">FIGHTER</div>
+					<div class="card-body">
+						<li>₹<span class="money">6600</span>/6 Mounth</li>
+						<li>Responsiive Website</li>
+						<li>Domain Name Free</li>
+						<li>Mobile Friendly</li>
+						<li>Webmail Support</li>
+						<li>Customer Support 24/7</li>
+					</div>
+					<div class="card-footer">
+						<a href="#">Purchase</a>
+					</div>
+				</div>
+				<div class="card">
+					<div class="card-header">WARRIOR</div>
+					<div class="card-body">
+						<li>₹<span class="money">13400</span>/12 Month</li>
+						<li>Responsiive Website</li>
+						<li>Domain Name Free</li>
+						<li>Mobile Friendly</li>
+						<li>Webmail Support</li>
+						<li>Customer Support 24/7</li>
+					</div>
+					<div class="card-footer">
+						<a href="#">Purchase</a>
+					</div>
+				</div>
+			</div>
+		</section>
+	</body>
+</html>
